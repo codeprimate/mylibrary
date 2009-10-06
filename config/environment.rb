adac451abdbe7b5070380bb2960c2d066232cb77
Patrick Morgan <patrick.morgan@masterwebdesign.net>
2009-10-06 09:48:15 -0500

    capistrano config; session secret config via application.yml

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,9 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   config.frameworks -= [ :action_mailer ]
   config.time_zone = 'UTC'
-  config.gem "sqlite3-ruby", :lib => "sqlite3"
   config.gem "aws-s3", :lib => "aws/s3"
   config.gem "authlogic"
   config.gem "binarylogic-searchlogic", :lib => "searchlogic"
   config.gem "binarylogic-settingslogic", :lib => "settingslogic"
+  config.gem "sqlite3-ruby", :lib => "sqlite3"
 end
